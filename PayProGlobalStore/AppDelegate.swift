ca3fc2d7d2ebcd30e851adc8a87f9585186e96ef
srinivas <srinivas@codezyng.com>
2020-06-11 09:18:51 +0530

    Initial Commit

---
+++ PayProGlobalStore/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PayProGlobalStore
+//
+//  Created by Srinivas Prabhu G on 11/06/20.
+//  Copyright © 2020 PayPro Global Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
